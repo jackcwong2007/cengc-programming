4090a613dd3f20f8b7cac533a312ef2bafe94bdb
Jack Wong <120768324+jackcwong2007@users.noreply.github.com>
2025-11-15 15:19:57 -0500

    Add files via upload

---
+++ 2025.py
@@ -0,0 +1,31 @@
+import base64
+
+hex_string = "54 68 65 54 72 00 75 74 68 49 1A 73 4E 7F 65 61 72"
+# remove spaces, convert hex string to bytes
+hex_string = hex_string.replace(" ", "")
+message_bytes = bytes.fromhex(hex_string)
+
+# keep only printable ASCII characters
+printable_bytes = [b for b in message_bytes if 32 <= b <= 126]
+
+#print characters
+for b in printable_bytes:
+    print(chr(b), end = "")
+print("")
+
+#m3
+base64_string = "MjMwMDAwLTMxMS0xMTU="
+decoded_bytes = base64.b64decode(base64_string)
+decoded_string = decoded_bytes.decode('utf-8')
+print(decoded_string)
+
+#m4
+nums = "178, 150, 38, 118, 150, 230, 22, 46, 130, 46, 46, 134, 198, 214, 12, 140, 44, 172".split(", ")
+binary = [format(int(n), "08b") for n in nums] #pads to 8 bits because for some reason it doesn't work otherwise!!!!
+reversed_binary = [b[::-1] for b in binary]
+ascii_chars = [chr(int(b, 2)) for b in reversed_binary]
+print("".join(ascii_chars))
+
+
+
+
